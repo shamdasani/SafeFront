26d4ded4958ad18056f965a0e2805175c1a665f7
Varun <vjindz@gmail.com>
2018-09-08 09:24:09 -0400

    added Power component

---
+++ src/pages/power.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Power = () => (
+  <div>
+    <h1>Power Outages</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default Power
